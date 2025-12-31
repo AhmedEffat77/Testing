233941dca6f159b2fb57685d06a6bbe4e532d180
AhmedEffat77 <74103440+AhmedEffat77@users.noreply.github.com>
2025-12-31 20:21:46 +0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCnK36b-6u16j9YTXYvzpJ_uR2_E6gOPM",
+  apiKey: "AIzaSyDCnK36b-6u16j9YTXYvzpj_uR2_E6gOPM",
   authDomain: "wh-management-11c29.firebaseapp.com",
   projectId: "wh-management-11c29",
   storageBucket: "wh-management-11c29.firebasestorage.app",
   messagingSenderId: "163490490102",
-  appId: "1:163490490102:web:650931c42fd54f699c2ed7"
+  appId: "1:163490490102:web:650931c42fd54f699c2ed7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// ✅ exports المطلوبة
+export const auth = getAuth(app);
+export const db = getFirestore(app);
