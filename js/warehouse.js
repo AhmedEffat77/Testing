6b1d9fc8b551d2221cb4eabaf42e6d7040298c0d
AhmedEffat77 <74103440+AhmedEffat77@users.noreply.github.com>
2026-01-01 13:10:36 +0400

    Delete js directory

--- js/warehouse.js
+++
@@ -1,9 +0,0 @@
-import { protectPage } from "./guard.js";
-protectPage("warehouse");
-
-import { auth } from "./firebase.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-document.getElementById("logoutBtn")?.addEventListener("click", async () => {
-  try { await signOut(auth); } finally { window.location.href = "index.html"; }
-});
